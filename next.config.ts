89b44365a20132fb0db372d1fbafef3440ac4a8d
Asaad Zein <160162342+asaadzx@users.noreply.github.com>
2025-09-03 01:58:54 +0300

    refactor: Remove unused configuration options from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,3 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: process.env.GITHUB_REPOSITORY?.split('/')[1] || '',
 };
