b099acee7880c2f56addf404aba1a4c4e718ea94
GhostFreakk <d3f4ultc0d3@gmail.com>
2025-09-02 10:14:18 +0300

    used github rather than vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: process.env.GITHUB_REPOSITORY?.split('/')[1] || '',
 };
-
-export default nextConfig;
